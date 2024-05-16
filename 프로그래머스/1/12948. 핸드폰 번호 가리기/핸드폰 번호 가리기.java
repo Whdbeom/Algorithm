93ac4b4cef09cc305c567628659b3ec043cc3a90
종범 <whdqja918@gmail.com>
2024-05-16 18:19:25 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.07 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String phone_number) {
-        String answer = "*".repeat(Math.max(0, phone_number.length() - 4)) + phone_number.substring(Math.max(0, phone_number.length() - 4));
-        return answer;
+      return "*".repeat(phone_number.length() - 4) + phone_number.substring(phone_number.length() - 4);
     }
 }
